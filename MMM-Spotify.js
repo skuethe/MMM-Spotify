d3d9beb3fc720b70098ce5841b546f02baf4a52a
eouia3rd <eouia@eouia3rdui-MBP.fritz.box>
2019-03-17 12:17:21 +0100

    add:progress bar

--- MMM-Spotify.js
+++ MMM-Spotify.js
@@ -64,6 +64,31 @@ Module.register("MMM-Spotify", {
     }
   },
 
+  updateProgress: function(current) {
+    var msToTime = (duration) => {
+      var ret = ""
+      var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24)
+      if (hours > 0) {
+        hours = (hours < 10) ? "0" + hours : hours
+        ret = ret + hours + ":"
+      }
+      minutes = (minutes < 10) ? "0" + minutes : minutes
+      seconds = (seconds < 10) ? "0" + seconds : seconds
+      return ret + minutes + ":" + seconds
+    }
+    var songDur = current.item.duration_ms
+    var cur = current.progress_ms
+    var pros = (cur / songDur) * 100
+
+    document.getElementById("SPOTIFY_PROGRESS_END").innerHTML = msToTime(songDur)
+    document.getElementById("SPOTIFY_PROGRESS_CURRENT").innerHTML = msToTime(cur)
+    document.getElementById("SPOTIFY_PROGRESS_BAR_NOW").style.width = pros + "%"
+
+  },
+
   updateCurrentPlayback: function(current) {
     if (!current) return
     var isChanged = false
@@ -76,7 +101,9 @@ Module.register("MMM-Spotify", {
     } else if (this.currentPlayback.device.id !== current.device.id) {
       isChanged = true
     } else if (this.currentPlayback.progress_ms !== current.progress_ms)  {
-      isChanged = true
+      //isChanged = true  //It would make too many updateDom.
+      //It's better to manipulate Dom directly
+      this.updateProgress(current)
     }
 
     if (isChanged) {
@@ -88,13 +115,6 @@ Module.register("MMM-Spotify", {
   getDom: function(){
     var m = document.createElement("div")
     m.id = "SPOTIFY"
-    if (this.currentPlayback) {
-      if (this.currentPlayback.is_playing) {
-        m.className = "playing"
-      } else {
-        m.className = "pausing"
-      }
-    }
 
     var back = document.createElement("div")
     back.id = "SPOTIFY_BACKGROUND"
@@ -108,12 +128,7 @@ Module.register("MMM-Spotify", {
 
     var cover_img = document.createElement("img")
     cover_img.id = "SPOTIFY_COVER_IMAGE"
-    if (this.currentPlayback) {
-      cover_img.src = this.currentPlayback.item.album.images[0].url
-      back.style.backgroundImage = `url(${this.currentPlayback.item.album.images[0].url})`
-    } else  {
-      cover_img.src = "./modules/MMM-Spotify/spotify-xxl.png"
-    }
+    cover_img.src = "./modules/MMM-Spotify/resources/spotify-xxl.png"
     cover.appendChild(cover_img)
     fore.appendChild(cover)
 
@@ -130,21 +145,48 @@ Module.register("MMM-Spotify", {
     device.id = "SPOTIFY_DEVICE"
 
     var progress = document.createElement("div")
-    progress_ms = "PROGRESS_BAR"
-    
+    progress.id = "SPOTIFY_PROGRESS"
+    var currentTime = document.createElement("div")
+    currentTime.id = "SPOTIFY_PROGRESS_CURRENT"
+    var songTime = document.createElement("div")
+    songTime.id = "SPOTIFY_PROGRESS_END"
+    var time = document.createElement("div")
+    time.id = "SPOTIFY_PROGRESS_TIME"
+    time.appendChild(currentTime)
+    time.appendChild(songTime)
+    progress.appendChild(time)
+    var bar = document.createElement("div")
+    bar.id = "SPOTIFY_PROGRESS_BAR"
+    var barNow = document.createElement("div")
+    barNow.id = "SPOTIFY_PROGRESS_BAR_NOW"
+    bar.appendChild(barNow)
+
+
+    progress.appendChild(bar)
+
+
 //    var time_ms = this.currentPlayback.progress_ms
-    
+
 
     if (this.currentPlayback) {
-      title.innerHTML = `<i class="fas fa-music"></i>` + " " + this.currentPlayback.item.name
-      artist.innerHTML = `<i class="fas fa-user-circle"></i>` + "  " + this.currentPlayback.item.artists[0].name
+      if (this.currentPlayback.is_playing) {
+        m.className = "playing"
+      } else {
+        m.className = "pausing"
+      }
+      if (this.currentPlayback.item) {
+        cover_img.src = this.currentPlayback.item.album.images[0].url
+        back.style.backgroundImage = `url(${this.currentPlayback.item.album.images[0].url})`
+        //progress.innerHTML  = `<i class="fas fa-clock"></i>` + " " +  Math.floor(this.currentPlayback.progress_ms / 60000) + ":" + (((this.currentPlayback.progress_ms % 60000) / 1000).toFixed(0)-1) + " / " + Math.floor(this.currentPlayback.item.duration_ms / 60000) + ":" + (((this.currentPlayback.item.duration_ms % 60000) / 1000).toFixed(0)-1)  }
+        title.innerHTML = `<i class="fas fa-music"></i>` + " " + this.currentPlayback.item.name
+        artist.innerHTML = `<i class="fas fa-user-circle"></i>` + "  " + this.currentPlayback.item.artists[0].name
+      }
       device.innerHTML = `<i class="fas fa-volume-up"></i>` + " " + this.currentPlayback.device.name
-      progress.innerHTML  = `<i class="fas fa-clock"></i>` + " " +  Math.floor(this.currentPlayback.progress_ms / 60000) + ":" + (((this.currentPlayback.progress_ms % 60000) / 1000).toFixed(0)-1) + " / " + Math.floor(this.currentPlayback.item.duration_ms / 60000) + ":" + (((this.currentPlayback.item.duration_ms % 60000) / 1000).toFixed(0)-1)  }
-
+    }
+    info.appendChild(progress)
     info.appendChild(title)
     info.appendChild(artist)
     info.appendChild(device)
-    info.appendChild(progress)
     fore.appendChild(info)
     m.appendChild(fore)
     return m
