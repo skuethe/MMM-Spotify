09d15df2c2cea8b15223883e6631d9e1192d3fa3
Humberto Virtudes <humberto.virtudes@gmail.com>
2020-05-11 02:29:32 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -137,7 +137,6 @@ module.exports = NodeHelper.create({
                 this.spotify.pause((code, error, result) => {
                     if ((code !== 204) && (code !== 202)){
                         //console.log(error)
-                        return
                     }
                     this.sendSocketNotification(allControlNotifications[noti], result)
                     return
