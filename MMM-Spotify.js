43c7af1c3e18fd29e50a687c606f64a1a284fc66
Swapnil Devesh <sid.swapnildevesh@gmail.com>
2022-01-28 03:57:41 +0530

    Update MMM-Spotify.js

--- MMM-Spotify.js
+++ MMM-Spotify.js
@@ -523,9 +523,7 @@ Module.register("MMM-Spotify", {
       display_name = playbackItem.show.name
     }
 
-    if (img_url) {
-      this.updateCoverImage(img_url)
-    }
+    this.updateCoverImage(img_url);
 
     const title = document.querySelector("#SPOTIFY_TITLE .text")
     title.textContent = playbackItem.name
